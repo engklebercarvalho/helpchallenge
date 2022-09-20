7b0adacd7e0f75164affa83a7b9a57a6197cfbbe
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-09-20 19:41:18 -0300

    Teste v2

--- IaC/azure-user-data-frontend.sh
+++ IaC/azure-user-data-frontend.sh
@@ -7,7 +7,7 @@ systemctl start apache2.service
 systemctl enable apache2.service
 ufw allow 80/tcp comment 'accept Apache'
 ufw allow 443/tcp comment 'accept HTTPS connections'
-mkdir /home/svc_Linux/PSI5120
-git clone https://github.com/engklebercarvalho/PSI5120/ /home/svc_Linux/PSI5120
+mkdir /home/svc_Linux/helpchallenge
+git clone https://github.com/engklebercarvalho/helpchallenge/ /home/svc_Linux/helpchallenge
 rm -rf /var/www/html/index.html
-cp /home/svc_Linux/PSI5120/frontazure/index.html /var/www/html/index.html
+cp /home/svc_Linux/helpchallenge/frontazure/index.html /var/www/html/index.html
